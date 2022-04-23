c22cc0444415eca05ec3bb92bc671c2d6575372d
Gajesh S <skg10408@gmail.com>
2022-04-23 23:27:35 +0530

    fix build error

--- Code/src/matmul/matmul.cpp
+++ Code/src/matmul/matmul.cpp
@@ -1,4 +1,4 @@
-#include "matmul.hpp"
+#include "matmul/matmul.hpp"
 
 #include "boundedWSDequeue.hpp"
 #include "unboundedWSDequeue.hpp"
@@ -6,20 +6,20 @@
 namespace matmul
 {
 
-    MatrixMultiplier::MatrixMultiplier(int num_threads, Matrix m1, Matrix m2, bool bounded_wsd = false)
+    MatrixMultiplier::MatrixMultiplier(int num_threads, Matrix m1, Matrix m2, bool bounded_wsd)
         : m_num_threads(num_threads),
           m_matrix1(m1),
           m_matrix2(m2)
     {
         if (bounded_wsd)
         {
-            for (int i = 0; i < m_num_threads; ++i)
-                m_wsdequeue.push_back(std::make_shared<WSD::BoundedWSDequeue>());
+            // for (int i = 0; i < m_num_threads; ++i)
+            // m_wsdequeue.push_back(std::make_shared<WSD::BoundedWSDequeue>());
         }
         else
         {
-            for (int i = 0; i < m_num_threads; ++i)
-                m_wsdequeue.push_back(std::make_shared<WSD::UnboundedWSDequeue>());
+            // for (int i = 0; i < m_num_threads; ++i)
+            // m_wsdequeue.push_back(std::make_shared<WSD::UnboundedWSDequeue>());
         }
     }
 
